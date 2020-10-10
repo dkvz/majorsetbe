39f7e2f3bf1afdeb619dab912ac035d789f26464
dkvz <github@dkvz.eu>
2020-10-10 22:47:36 +0200

    Before ruining everything

--- _resources/custom_widgets/social-icons/social-icons.php
+++ _resources/custom_widgets/social-icons/social-icons.php
@@ -1,6 +1,6 @@
 <?
 // Creating the widget 
-class wpb_widget extends WP_Widget {
+class ms_social_icons_widget extends WP_Widget {
   
   function __construct() {
     parent::__construct(      
@@ -9,32 +9,34 @@ class wpb_widget extends WP_Widget {
       // Widget name will appear in UI
       __('Major Set social icons', 'ms_social_icons_domain'), 
       // Widget description
-      array( 'description' => __( 'Widget for displaying social media links', 'ms_social_icons_domain' ), ) 
+      array( 
+        'description' => __( 'Widget for displaying social media links', 'ms_social_icons_domain' ), 
+      ) 
     );
   }
     
   // Creating widget front-end
-    
   public function widget( $args, $instance ) {
-    $title = apply_filters( 'widget_title', $instance['title'] );
-      
+    //$title = apply_filters( 'widget_title', $instance['title'] );
+    
     // before and after widget arguments are defined by themes
     echo $args['before_widget'];
-    if ( ! empty( $title ) )
-    echo $args['before_title'] . $title . $args['after_title'];
-      
+    //if ( ! empty( $title ) )
+    //echo $args['before_title'] . $title . $args['after_title'];
+    
     // This is where you run the code and display the output
-    echo __( 'Hello, World!', 'wpb_widget_domain' );
+    echo __( 'SOCIAL ICONS SHOULD BE HERE', 'ms_social_icons_domain' );
     echo $args['after_widget'];
   }
-            
+   
+  /*
   // Widget Backend 
   public function form( $instance ) {
     if ( isset( $instance[ 'title' ] ) ) {
       $title = $instance[ 'title' ];
     }
     else {
-      $title = __( 'New title', 'wpb_widget_domain' );
+      $title = __( 'New title', 'ms_social_icons_domain' );
     }
     // Widget admin form
     ?>
@@ -44,19 +46,22 @@ class wpb_widget extends WP_Widget {
     </p>
     <?php 
   }
-        
+  */
+  
+  /*
   // Updating widget replacing old instances with new
   public function update( $new_instance, $old_instance ) {
     $instance = array();
     $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
     return $instance;
   }
+  */
 
 } 
  
  
 // Register and load the widget
-function wpb_load_widget() {
-  register_widget( 'wpb_widget' );
+function ms_load_widget() {
+  register_widget( 'ms_social_icons_widget' );
 }
-add_action( 'widgets_init', 'wpb_load_widget' );
+add_action( 'widgets_init', 'ms_load_widget' );
