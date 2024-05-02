3ead16a076641b05126852f21b4b2e7d6fa295f3
DkVZ <github@dkvz.eu>
2024-05-02 16:42:47 +0200

    Invert order of past events for event calendar

--- functions.php
+++ functions.php
@@ -323,3 +323,23 @@ function ms_load_widget() {
   register_widget( 'ms_social_icons_widget' );
 }
 add_action( 'widgets_init', 'ms_load_widget' );
+
+/**
+ * Changes Past Event Reverse Chronological Order
+ *
+ * @param array $template_vars An array of variables used to display the current view.
+ *
+ * @return array Same as above. 
+ */
+function tribe_past_reverse_chronological_v2( $template_vars ) {
+ 
+  if ( ! empty( $template_vars['is_past'] ) ) {
+    $template_vars['events'] = array_reverse( $template_vars['events'] );
+  }
+ 
+  return $template_vars;
+}
+// Change List View to Past Event Reverse Chronological Order 
+add_filter( 'tribe_events_views_v2_view_list_template_vars', 'tribe_past_reverse_chronological_v2', 100 );
+// Change Photo View to Past Event Reverse Chronological Order
+add_filter( 'tribe_events_views_v2_view_photo_template_vars', 'tribe_past_reverse_chronological_v2', 100 );
